b4d6f97501c6205695b27f937246d0c88abccfc4
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2023-08-10 22:21:42 +0900

    Initial commit

---
+++ test/aws-cdk-sechub-ssm.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkSechubSsm from '../lib/aws-cdk-sechub-ssm-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-sechub-ssm-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkSechubSsm.AwsCdkSechubSsmStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
