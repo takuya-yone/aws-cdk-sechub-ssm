fa0f651e56adab1a39749708cb18af5d8a43c8c0
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2023-08-11 06:09:22 +0900

    up

--- src/lambda/index.ts
+++ src/lambda/index.ts
@@ -1,6 +1,15 @@
 import { Handler, Context, SQSEvent } from 'aws-lambda';
 import { SSMClient } from '@aws-sdk/client-ssm'; // ES Modules import
 
+interface OpsItemProps {
+  Description: string;
+  RelatedOpsItems: [{ OpsItemId: string }];
+  Source: string;
+  Title: string;
+  Tags: { Key: string; Value: string };
+}
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/ssm/command/CreateOpsItemCommand/
+
 function getFinding(event: SQSEvent): object {
   const _tmp = JSON.parse(event.Records[0].body).Message;
   const _result = JSON.parse(_tmp).detail.findings[0] as object;
@@ -15,7 +24,6 @@ export const handler: Handler = async (event: SQSEvent, context: Context) => {
   console.log(typeof finding);
   // console.log(finding.Compliance);
   const ssmClient = new SSMClient({});
-  console.log(ssmClient);
-
+  // console.log(ssmClient);
   return null;
 };
