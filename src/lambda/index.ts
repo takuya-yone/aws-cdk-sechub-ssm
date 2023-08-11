955faf5edbeac21b54ac8cc7dbf8603e3d756632
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2023-08-12 00:01:18 +0900

    up

--- src/lambda/index.ts
+++ src/lambda/index.ts
@@ -13,10 +13,26 @@ interface OpsItemProps {
 }
 // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/ssm/command/CreateOpsItemCommand/
 
-function getFinding(event: SQSEvent): object {
+interface SecHubASFF {
+  AwsAccountId: string;
+  CreatedAt: string;
+  Description: string;
+  GeneratorId: string;
+  Id: string;
+  ProductArn: string;
+  Resouces: [object];
+  SchemaVersion: string;
+  ProductName?: string;
+  Region?: string;
+  Remendiation?: { Recommendation: { Text: string; Url: string } };
+  SourceUrl?: string;
+}
+// https://docs.aws.amazon.com/ja_jp/securityhub/latest/userguide/asff-required-attributes.html
+
+function getFinding(event: SQSEvent): SecHubASFF {
   const _tmp = JSON.parse(event.Records[0].body).Message;
-  const _result = JSON.parse(_tmp).detail.findings[0] as object;
-  return _result;
+  const result = JSON.parse(_tmp).detail.findings[0] as object;
+  return result as SecHubASFF;
 }
 
 const tracer = new Tracer();
@@ -24,9 +40,18 @@ const logger = new Logger();
 
 export const handler: Handler = async (event: SQSEvent, context: Context) => {
   tracer.getSegment();
-  const finding = getFinding(event);
-  logger.info(JSON.stringify(finding, null, 2));
-  logger.info(typeof finding);
+  const finding: SecHubASFF = getFinding(event);
+  // console.log(JSON.stringify(finding, null, 2));
+
+  if (finding.ProductName === 'GuardDuty') {
+    console.log('guardduty');
+    console.log(finding.SourceUrl);
+  }
+  if (finding.ProductName === 'Config') {
+    console.log('Config');
+  }
+
+  // logger.info(typeof finding);
   // logger.info(finding.Compliance);
   const ssmClient = new SSMClient({});
   // logger.info(ssmClient);
