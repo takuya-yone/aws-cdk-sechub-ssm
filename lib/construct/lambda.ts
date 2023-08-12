7ccda9690a08b85d5bbfe2869d2f5aa800783e26
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2023-08-13 05:43:18 +0900

    up

--- lib/construct/lambda.ts
+++ lib/construct/lambda.ts
@@ -4,6 +4,8 @@ import { Construct } from 'constructs';
 import { aws_lambda as lambda } from 'aws-cdk-lib';
 import { aws_logs as logs } from 'aws-cdk-lib';
 import { aws_lambda_nodejs as node_lambda } from 'aws-cdk-lib';
+import { aws_iam as iam } from 'aws-cdk-lib';
+import { ScopedAws } from 'aws-cdk-lib';
 
 import * as path from 'path';
 
@@ -11,10 +13,28 @@ export class LambdaConstruct extends Construct {
   public sechubSsmFunctin: node_lambda.NodejsFunction;
   constructor(scope: Construct, id: string) {
     super(scope, id);
+    const { accountId } = new ScopedAws(this);
+
+    const sechubSsmFunctinRole = new iam.Role(this, 'SechubSsmFunctinRole', {
+      roleName: 'SechubSsmFunctinRole',
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName(
+          'service-role/AWSLambdaBasicExecutionRole',
+        ),
+        iam.ManagedPolicy.fromAwsManagedPolicyName(
+          'service-role/AWSLambdaSQSQueueExecutionRole',
+        ),
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AWSXrayWriteOnlyAccess'),
+        iam.ManagedPolicy.fromAwsManagedPolicyName(
+          'AWSIncidentManagerResolverAccess',
+        ),
+      ],
+    });
 
     this.sechubSsmFunctin = new node_lambda.NodejsFunction(
       this,
-      'sechubSsmFunctin',
+      'SechubSsmFunctin',
       {
         runtime: lambda.Runtime.NODEJS_18_X,
         entry: path.join(__dirname, '../../src/lambda/index.ts'),
@@ -24,11 +44,12 @@ export class LambdaConstruct extends Construct {
         tracing: lambda.Tracing.ACTIVE,
         retryAttempts: 0,
         logRetention: logs.RetentionDays.THREE_DAYS,
+        role: sechubSsmFunctinRole,
 
         // insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_229_0,
-        // environment: {
-        //   // DDB_TABLE: props.tableName,
-        // },
+        environment: {
+          RESPONSE_PLAN_ARN: `arn:aws:ssm-incidents::${accountId}:response-plan/test-t-yonezawa-plan`,
+        },
       },
     );
   }
--- src/lambda/index.ts
+++ src/lambda/index.ts
@@ -2,18 +2,15 @@ import { Handler, Context, SQSEvent } from 'aws-lambda';
 import { Tracer } from '@aws-lambda-powertools/tracer';
 import { Logger } from '@aws-lambda-powertools/logger';
 
-import { SSMClient } from '@aws-sdk/client-ssm';
+import {
+  SSMIncidentsClient,
+  StartIncidentCommand,
+  StartIncidentInput,
+  UpdateIncidentRecordCommand,
+  UpdateIncidentRecordInput,
+} from '@aws-sdk/client-ssm-incidents';
 
 type Severity = 'INFORMATIONAL' | 'LOW' | 'MEDIUM' | 'HIGH' | 'CRITICAL';
-
-type OpsItemProps = {
-  Description: string;
-  RelatedOpsItems: [{ OpsItemId: string }];
-  Source: string;
-  Title: string;
-  Tags: { Key: string; Value: string };
-};
-// https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/ssm/command/CreateOpsItemCommand/
 
 type SecHubASFF = {
   AwsAccountId: string;
@@ -24,14 +21,69 @@ type SecHubASFF = {
   ProductArn: string;
   Resouces: [object];
   SchemaVersion: string;
+  Title: string;
   ProductName?: string;
   Region?: string;
   Remendiation?: { Recommendation: { Text: string; Url: string } };
   SourceUrl?: string;
   Workflow?: { Status: string };
-  Severity?: { Label: Severity; Original: Severity };
+  Severity: { Label: Severity; Original: Severity };
 };
 // https://docs.aws.amazon.com/ja_jp/securityhub/latest/userguide/asff-required-attributes.html
+
+type impactLevel = 1 | 2 | 3 | 4 | 5;
+
+// type StartIncidentInput = {
+//   responsePlanArn: string | undefined;
+//   title: string;
+
+//   impact: impactLevel;
+// };
+//https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/ssm-incidents/command/StartIncidentCommand/
+
+// type StartIncidentInput = {
+//   responsePlanArn: string | undefined;
+//   title: string;
+
+//   impact: impactLevel;
+// };
+
+function getImpactFromSeverity(label: string): impactLevel {
+  if (label === 'CRITICAL') {
+    return 1;
+  } else if (label === 'HIGH') {
+    return 2;
+  } else if (label === 'MEDIUM') {
+    return 3;
+  } else if (label === 'LOW') {
+    return 4;
+  } else {
+    return 5;
+  }
+}
+
+async function createIncident(finding: SecHubASFF): Promise<void> {
+  const client = new SSMIncidentsClient({});
+
+  const startIncidentInput: StartIncidentInput = {
+    responsePlanArn: process.env['RESPONSE_PLAN_ARN'],
+    title: finding.Title,
+    impact: getImpactFromSeverity(finding.Severity.Label),
+  };
+  const startIncidentCommand = new StartIncidentCommand(startIncidentInput);
+  const incidentRecordArn = (await client.send(startIncidentCommand))
+    .incidentRecordArn;
+
+  const updateIncidentRecordInput: UpdateIncidentRecordInput = {
+    arn: incidentRecordArn,
+    summary: `# SourceUrl\n${finding.SourceUrl}\n# Description\n${finding.Description}`,
+  };
+  const updateIncidentRecordCommand = new UpdateIncidentRecordCommand(
+    updateIncidentRecordInput,
+  );
+  const response = await client.send(updateIncidentRecordCommand);
+  console.log(response);
+}
 
 function getFinding(event: SQSEvent): SecHubASFF {
   const _tmp = JSON.parse(event.Records[0].body).Message;
@@ -45,31 +97,44 @@ const logger = new Logger();
 export const handler: Handler = async (event: SQSEvent, context: Context) => {
   tracer.getSegment();
   const finding: SecHubASFF = getFinding(event);
-  // console.log(JSON.stringify(finding, null, 2));
+  console.log(JSON.stringify(finding, null, 2));
 
-  if (finding.ProductName === 'GuardDuty') {
-    console.log('guardduty');
-    console.log(finding.Severity);
-    console.log(finding);
+  if (
+    finding.ProductName === 'GuardDuty' &&
+    finding.Workflow?.Status === 'NEW'
+  ) {
+    console.log('GuardDuty');
+    // console.log(finding.Severity);
+    // console.log(finding);
+
+    createIncident(finding);
+
     // console.log(finding.SourceUrl);
     // console.log(finding.AwsAccountId);
-  } else if (finding.ProductName === 'Security Hub') {
+  } else if (
+    finding.ProductName === 'Security Hub' &&
+    finding.Workflow?.Status === 'NEW'
+  ) {
     console.log('Security Hub');
-    console.log(finding.Severity);
-    console.log(finding);
-  } else if (finding.ProductName === 'Config') {
+    // console.log(finding.Severity);
+    // console.log(finding);
+  } else if (
+    finding.ProductName === 'Config' &&
+    finding.Workflow?.Status === 'NEW'
+  ) {
     console.log('Config');
-    console.log(finding.Severity);
-    console.log(finding);
+    // console.log(finding.Severity);
+    // console.log(finding);
     // console.log(finding.SourceUrl);
     // console.log(finding.AwsAccountId);
   } else {
     console.log('Else');
-    console.log(finding);
+    // console.log(finding);
   }
   // logger.info(typeof finding);
   // logger.info(finding.Compliance);
-  const ssmClient = new SSMClient({});
+  // const ssmClient = new SSMClient({});
+
   // logger.info(ssmClient);
   return null;
 };
